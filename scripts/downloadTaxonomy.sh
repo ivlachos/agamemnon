60618e15f012872315a645399de68658b965a7a8
gskoufos <32651024+gskoufos@users.noreply.github.com>
2021-06-25 21:40:07 +0300

    Update downloadTaxonomy.sh

--- scripts/downloadTaxonomy.sh
+++ scripts/downloadTaxonomy.sh
@@ -9,29 +9,29 @@ fi
 echo "*** [Downloading NCBI Taxonomy]"
 
 while [ ! -f  ../Taxonomy/dead_nucl.accession2taxid ]; do
-	wget -P ../Taxonomy/ ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/dead_nucl.accession2taxid.gz
+	wget -P ../Taxonomy/ https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/dead_nucl.accession2taxid.gz
 	gunzip ../Taxonomy/dead_nucl.accession2taxid.gz
 done
 
 while [ ! -f  ../Taxonomy/dead_wgs.accession2taxid ]; do
-	wget -P ../Taxonomy/ ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/dead_wgs.accession2taxid.gz
+	wget -P ../Taxonomy/ https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/dead_wgs.accession2taxid.gz
 	gunzip ../Taxonomy/dead_wgs.accession2taxid.gz
 done
 
 while [ ! -f  ../Taxonomy/nucl_gb.accession2taxid ]; do
-	wget -P ../Taxonomy/ ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
+	wget -P ../Taxonomy/ https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
 	gunzip ../Taxonomy/nucl_gb.accession2taxid.gz
 done
 
 while [ ! -f  ../Taxonomy/nucl_wgs.accession2taxid ]; do
-	wget -P ../Taxonomy/ ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.gz
+	wget -P ../Taxonomy/ https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.gz
 	gunzip ../Taxonomy/nucl_wgs.accession2taxid.gz
 done
 
 
 
 while [ ! -f  ../Taxonomy/rankedlineage.dmp ]; do
-	wget -P ../Taxonomy/ ftp://ftp.ncbi.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
+	wget -P ../Taxonomy/ https://ftp.ncbi.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
 	tar -zxvf ../Taxonomy/new_taxdump.tar.gz rankedlineage.dmp
 	tar -zxvf ../Taxonomy/new_taxdump.tar.gz nodes.dmp
 	tar -zxvf ../Taxonomy/new_taxdump.tar.gz names.dmp
